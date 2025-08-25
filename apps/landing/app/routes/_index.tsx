bbe597cfd91577f9283a1caec39f975a971a5c5f
hiepthai <thaichanhiep@gmail.com>
2025-08-25 22:22:22 +0700

    feat: add logo

--- apps/landing/app/routes/_index.tsx
+++ apps/landing/app/routes/_index.tsx
@@ -1,46 +1,19 @@
 import type { MetaFunction } from '@remix-run/cloudflare';
-import { data } from '@remix-run/cloudflare';
-import { useLoaderData } from '@remix-run/react';
 
-import { getApi } from '../../lib/api';
+import { AveonLogo } from '~/components/logo';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: 'New Remix App' },
-    { name: 'description', content: 'Welcome to Remix!' },
+    { title: 'Aveon App' },
+    { name: 'description', content: 'AI Learning Companion' },
   ];
 };
 
-interface LoaderData {
-  message?: string;
-}
-
-export async function loader() {
-  const api = getApi();
-
-  const res = await api.hello.$post({ json: { name: 'Tom' } });
-
-  let json;
-
-  if (res.ok) {
-    json = await res.json();
-  }
-
-  return data<SuccessJsonResponse<LoaderData>>(
-    { ok: true, data: { message: json?.message } },
-    {},
-  );
-}
-
 export default function Index() {
-  const {
-    data: { message },
-  } = useLoaderData<SuccessJsonResponse<LoaderData>>();
-
   return (
     <div className="flex h-screen items-center justify-center">
       <div className="flex flex-col items-center gap-16">
-        <h1>{message}</h1>
+        <AveonLogo className="w-xs fill-gray-500 dark:fill-gray-200" />
       </div>
     </div>
   );
