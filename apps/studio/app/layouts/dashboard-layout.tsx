e6ffc90dffbd8887dfd222d3e6e0bc739c80613b
hiepthai <thaichanhiep@gmail.com>
2025-08-29 15:41:08 +0700

    feat: add breadcrumb

--- apps/studio/app/layouts/dashboard-layout.tsx
+++ apps/studio/app/layouts/dashboard-layout.tsx
@@ -1,7 +1,15 @@
 import type { ReactElement } from 'react';
 
 import { AppSidebar } from '~/components/app-sidebar';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from '~/components/ui/breadcrumb';
 import { SidebarProvider, SidebarTrigger } from '~/components/ui/sidebar';
+import { Separator } from '~/components/ui/separator';
 
 export default function DashboardLayout({
   children,
@@ -12,7 +20,17 @@ export default function DashboardLayout({
     <SidebarProvider>
       <AppSidebar />
       <main className="w-full bg-[#f6f7fb] py-6 px-4 sm:px-6 lg:px-8 space-y-4">
-        <SidebarTrigger />
+        <header className="flex h-5 items-center gap-3">
+          <SidebarTrigger />
+          <Separator orientation="vertical" />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem>
+                <BreadcrumbLink href="/">Dashboard</BreadcrumbLink>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </header>
         {children}
       </main>
     </SidebarProvider>
